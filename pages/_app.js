8e3814b0de6c3c97217dcadd585d6a1f195de414
Daniel Beck <d.danielbeck@googlemail.com>
2022-02-26 19:38:30 +0100

    _app.js is not necessary anymore

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "../layout/style.css";
+
+// _app.js
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+export default MyApp;
